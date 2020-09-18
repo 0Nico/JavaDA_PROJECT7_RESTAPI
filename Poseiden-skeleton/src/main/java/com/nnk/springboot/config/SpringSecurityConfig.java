0b2955e94c26d3e2404969c9acdf408da7b61242
Nicolas Dupuy <nicolas.dupuy@mimbus.com>
2020-09-18 17:43:30 +0200

    Add security config

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.nnk.springboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.nnk.springboot.services.UserService;
+
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	 @Autowired
+     private UserService userDetailsService;
+	
+	 @Override
+	 protected void configure(HttpSecurity http) throws Exception {
+		 
+	    http.authorizeRequests()
+	            .antMatchers("/bidList/**", "/rating/**", "/ruleName/**", "/trade/**", "/curvePoint/**").hasAnyAuthority("ADMIN", "USER")
+	            .antMatchers("/user/**").permitAll()
+	            .and().formLogin()  
+	            .defaultSuccessUrl("/bidList/list")
+	            .and().logout()    
+	            .logoutUrl("/app-logout")
+	            .logoutSuccessUrl("/")
+	            .and().exceptionHandling()
+	            .accessDeniedPage("/app/error");
+	    http.sessionManagement()
+	            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+	            .maximumSessions(1);
+	}
+	 
+	 @Autowired
+	 public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		 BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		 auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+
+	 }
+}
